23969a6a89169d28f16814b937477c3d18e56111
niharika <niharikamadke@gmail.com>
2022-03-16 12:04:16 +0530

    deploying github-search-app

---
+++ src/Config/firebaseConfig.js
@@ -0,0 +1,10 @@
+export default {
+    apiKey: "AIzaSyCfwPTijBXTixG_LZuQv-YyFJfgVNETDYs",
+    authDomain: "github-search-app-f2f0c.firebaseapp.com",
+    projectId: "github-search-app-f2f0c",
+    storageBucket: "github-search-app-f2f0c.appspot.com",
+    messagingSenderId: "155523845918",
+    appId: "1:155523845918:web:43dbbf90f27408b6c2030f",
+    measurementId: "G-00W6406P6S"
+  };
+  
---
+++ src/layout/Footer.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Container } from "reactstrap";
+
+
+
+
+const Footer = () => {
+    return(
+        <Container 
+        fluid
+        tag="footer"
+        className="text-center bg-info text-uppercase fixed-bottom p-3"
+        >
+            Github search App with Firebase
+        </Container>
+    )
+
+}
+
+
+export default Footer
